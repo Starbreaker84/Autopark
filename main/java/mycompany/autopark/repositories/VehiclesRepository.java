2cd0f6c0fb62af14e89eb7fe78d885530b9b86fd
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2022-11-08 14:50:20 +0300

    Delete main/java/mycompany/autopark directory

--- main/java/mycompany/autopark/repositories/VehiclesRepository.java
+++
@@ -1,9 +0,0 @@
-package mycompany.autopark.repositories;
-
-import mycompany.autopark.models.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehiclesRepository extends JpaRepository<Vehicle, Integer> {
-}
